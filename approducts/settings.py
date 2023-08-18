5168a299861c7933a3a9870e58c980fdd273087c
Sanjar Samadov <sanjarbek2709@gmail.com>
2023-08-18 15:44:51 +0500

    remove .idea file and create .gitignore, changed django static file

--- approducts/settings.py
+++ approducts/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@szr_m_3_0@m46$qg93a2$^$#+0)=%6g@m459-4v_$=a*ih0ru
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'approducts.uz']
 
 
 # Application definition
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
